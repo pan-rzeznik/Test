432a65e30aea337b88df7189c0a57f56cd512f98
Mateusz <matimouzk@gmail.com>
2021-03-20 14:38:01 +0100

    valueAccount

--- src/app/components/create-accounts/create-accounts.component.ts
+++ src/app/components/create-accounts/create-accounts.component.ts
@@ -25,7 +25,7 @@ placeholderText;
 inputEvent = new EventEmitter
 
 @Input()
-cos='sdsdsd';
+valueAccount='sd';
 
 addUser(s){
   if(s.value.length>0){
@@ -35,7 +35,7 @@ addUser(s){
 }
 
 clearField(){
-   this.cos= ' '
+   this.valueAccount= ' '
    console.warn("dasda")
 }
 
